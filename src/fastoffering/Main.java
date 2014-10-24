ed10656d89b6207c6a2681ca4f9c1b75c535b9d1
Nate Seeley <seeleyn@adaptivecomputing.com>
2014-10-23 20:31:00 -0600

    Added Main class for Fast Offering app

---
+++ src/fastoffering/Main.java
@@ -0,0 +1,28 @@
+package fastoffering;
+
+import java.util.List;
+
+import utils.ParsingUtils;
+import fhe.Apartment;
+import fhe.Person;
+
+public class Main {
+
+	/**
+	 * @param args
+	 *            the command line arguments
+	 */
+	public static void main(String[] args) throws Exception {
+		if (args.length < 1) {
+			System.err.println("Usage: <input csv>");
+			System.err.println("Example  ./fhe sampleInput/wardList.csv");
+			System.exit(1);
+		}
+		String inputFile = args[0];
+		List<Person> persons = ParsingUtils.parseFile(inputFile);
+		System.out.println("read in " + persons.size() + " persons");
+		List<Apartment> apts = ParsingUtils.putIntoApts(persons);
+		Apartment.printApts(apts);
+	}
+
+}
