1355b6858969bba608df8ac3ca7965ba9c1d46f9
Nate Seeley <seeleyn@adaptivecomputing.com>
2014-11-29 20:53:07 -0700

    Updated unit tests

--- test/fhe/AddressTests.java
+++ test/fhe/AddressTests.java
@@ -1,9 +1,118 @@
 package fhe;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.junit.*;
+
 import static org.junit.Assert.*;
 
 public class AddressTests {
+
+	@Test
+	public void testCompareTo() {
+		List<Address> addresses = new ArrayList<Address>();
+		Address a0 = new Address("900 Main # D");
+		Address a1 = new Address("968 S 1000 E");
+		Address a2 = new Address("101 Elm Street, Springville, UT");
+		Address a3 = new Address("961 Main # C");
+		Address a4 = new Address("961 Main Apt A");
+		Address a5 = new Address("100 Elm Street, Springville, UT");
+		Address a6 = new Address("450 E 2000 S");
+		Address a7 = new Address("961 Main Apt B");
+		Address a8 = new Address("500 E 1000 S");
+
+		addresses.add(a0);
+		addresses.add(a1);
+		addresses.add(a2);
+		addresses.add(a3);
+		addresses.add(a4);
+		addresses.add(a5);
+		addresses.add(a6);
+		addresses.add(a7);
+		addresses.add(a8);
+
+		Collections.sort(addresses);
+		assertEquals(addresses.indexOf(a8), 0);
+		assertEquals(addresses.indexOf(a6), 1);
+		assertEquals(addresses.indexOf(a0), 2);
+		assertEquals(addresses.indexOf(a4), 3);
+		assertEquals(addresses.indexOf(a7), 4);
+		assertEquals(addresses.indexOf(a3), 5);
+		assertEquals(addresses.indexOf(a1), 6);
+		assertEquals(addresses.indexOf(a5), 7);
+		assertEquals(addresses.indexOf(a2), 8);
+	}
+
+	@Test
+	public void testCompareTo2() {
+		Address a0 = new Address("900 Main");
+		Address a1 = new Address("900 Main");
+		assertEquals(0, a0.compareTo(a1));
+	}
+
+	@Test
+	public void testCompareTo3() {
+		List<Address> addresses = new ArrayList<Address>();
+
+		Address a0 = new Address("1000 Center St");
+		Address a1 = new Address("9 Center St");
+		Address a2 = new Address("10000 Center St");
+		Address a3 = new Address("99 Center St");
+		Address a4 = new Address("100 Center St");
+		Address a5 = new Address("9999 Center St");
+		Address a6 = new Address("999 Center St");
+		Address a7 = new Address("10 Center St");
+
+		addresses.add(a0);
+		addresses.add(a1);
+		addresses.add(a2);
+		addresses.add(a3);
+		addresses.add(a4);
+		addresses.add(a5);
+		addresses.add(a6);
+		addresses.add(a7);
+
+		Collections.sort(addresses);
+		assertEquals(addresses.indexOf(a1), 0);
+		assertEquals(addresses.indexOf(a7), 1);
+		assertEquals(addresses.indexOf(a3), 2);
+		assertEquals(addresses.indexOf(a4), 3);
+		assertEquals(addresses.indexOf(a6), 4);
+		assertEquals(addresses.indexOf(a0), 5);
+		assertEquals(addresses.indexOf(a5), 6);
+		assertEquals(addresses.indexOf(a2), 7);
+	}
+
+	@Test
+	public void testCompareTo4() {
+		List<Address> addresses = new ArrayList<Address>();
+		Address a0 = new Address(0, null, "A", City.PROVO);
+		Address a1 = new Address(1, "Aardvark", "A", City.PROVO);
+		Address a2 = new Address(0, null, null, null);
+		Address a3 = new Address(0, "Aardvark", "A", City.PROVO);
+		Address a4 = new Address(0, null, null, City.PROVO);
+		Address a5 = new Address(0, null, null, null);
+		Address a6 = new Address(0, "Aardvark", null, City.PROVO);
+
+		addresses.add(a0);
+		addresses.add(a1);
+		addresses.add(a2);
+		addresses.add(a3);
+		addresses.add(a4);
+		addresses.add(a5);
+		addresses.add(a6);
+
+		Collections.sort(addresses);
+		assertEquals(true, addresses.indexOf(a5) == 0 || addresses.indexOf(a5) == 1);
+		assertEquals(true, addresses.indexOf(a2) == 0 || addresses.indexOf(a2) == 1);
+		assertEquals(addresses.indexOf(a4), 2);
+		assertEquals(addresses.indexOf(a0), 3);
+		assertEquals(addresses.indexOf(a6), 4);
+		assertEquals(addresses.indexOf(a3), 5);
+		assertEquals(addresses.indexOf(a1), 6);
+	}
 
 	@Test
 	public void testEquals() {
--- test/fhe/UtilsTests.java
+++ test/fhe/UtilsTests.java
@@ -1,33 +1,35 @@
 package fhe;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import utils.StringUtils;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
 public class UtilsTests {
 
 	@Test
 	public void stringEqualsTests() {
-		assertTrue(Utils.stringEquals(null,null));
-		assertTrue(Utils.stringEquals("1","1"));
-		assertTrue(Utils.stringEquals("a","a"));
-		
-		assertFalse(Utils.stringEquals(null,"1"));
-		assertFalse(Utils.stringEquals("1",null));
-		assertFalse(Utils.stringEquals("1","2"));
-		assertFalse(Utils.stringEquals("a", "A"));
+		assertTrue(StringUtils.stringEquals(null, null));
+		assertTrue(StringUtils.stringEquals("1", "1"));
+		assertTrue(StringUtils.stringEquals("a", "a"));
+
+		assertFalse(StringUtils.stringEquals(null, "1"));
+		assertFalse(StringUtils.stringEquals("1", null));
+		assertFalse(StringUtils.stringEquals("1", "2"));
+		assertFalse(StringUtils.stringEquals("a", "A"));
 	}
-	
+
 	@Test
 	public void stringEqualsIgnoreCaseTests() {
-		assertTrue(Utils.stringEqualsIgnoreCase(null,null));
-		assertTrue(Utils.stringEqualsIgnoreCase("1","1"));
-		assertTrue(Utils.stringEqualsIgnoreCase("a","a"));
-		assertTrue(Utils.stringEqualsIgnoreCase("a", "A"));
-		
-		assertFalse(Utils.stringEqualsIgnoreCase(null,"1"));
-		assertFalse(Utils.stringEqualsIgnoreCase("1",null));
-		assertFalse(Utils.stringEqualsIgnoreCase("1","2"));
+		assertTrue(StringUtils.stringEqualsIgnoreCase(null, null));
+		assertTrue(StringUtils.stringEqualsIgnoreCase("1", "1"));
+		assertTrue(StringUtils.stringEqualsIgnoreCase("a", "a"));
+		assertTrue(StringUtils.stringEqualsIgnoreCase("a", "A"));
+
+		assertFalse(StringUtils.stringEqualsIgnoreCase(null, "1"));
+		assertFalse(StringUtils.stringEqualsIgnoreCase("1", null));
+		assertFalse(StringUtils.stringEqualsIgnoreCase("1", "2"));
 
 	}
-	
+
 }
