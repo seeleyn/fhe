2ca38c3834c28f1704dccf33171f45b4143b4381
Nate Seeley <seeleyn@adaptivecomputing.com>
2016-07-01 20:23:49 -0600

    Got simple pdf written using itext

--- src/main/java/fastoffering/PdfPractice.java
+++ src/main/java/fastoffering/PdfPractice.java
@@ -1,8 +1,15 @@
 package fastoffering;
 
-import org.pegdown.Extensions;
-import org.pegdown.PegDownProcessor;
+//import org.pegdown.Extensions;
+//import org.pegdown.PegDownProcessor;
 import utils.FileUtils;
+
+import java.io.FileOutputStream;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+
 
 /**
  * Created by seeleyn on 7/1/16.
@@ -10,7 +17,7 @@ import utils.FileUtils;
 public class PdfPractice {
 
 	public static void main(String[] args) throws Exception {
-
+/*
 		String documentation = FileUtils.readFileIntoString(args[0]);
 
 		System.out.println(documentation);
@@ -19,5 +26,14 @@ public class PdfPractice {
 		String html = processor.markdownToHtml(documentation);
 		System.out.println("\n\n\n");
 		System.out.println(html);
+*/
+		Document document = new Document();
+		PdfWriter.getInstance(document,
+				new FileOutputStream("/tmp/HelloWorld.pdf"));
+
+		document.open();
+		document.add(new Paragraph("A Hello World PDF document."));
+		document.close(); // no need to close PDFwriter?
+
 	}
 }
